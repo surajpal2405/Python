3f91c4a9a3b2907ab28b0daef966456a7820140e
Proton <surajpal2405@gmail.com>
2021-07-01 22:21:52 +0530

    renamed the file test

---
+++ faulty_calculator.py
@@ -0,0 +1,18 @@
+ #faulty calculator of below operations only and regular for other opeations
+
+#45 *  = 555, 56 + 9 = 77, 56 / 6 = 4
+num1=int(input("enter num1"))
+op=input("+ / - *")
+num2=int(input("enter num2"))
+if num1==45 and op=="*" and num2==3:
+    print("555")
+elif num1==56 and op=="+" and num2==9:
+    print("77")
+elif num1==56 and op=="/" and num2==6:
+    print("4")
+elif op=="*":
+    print(int(num1) * num2)
+elif op=="+":
+    print(int(num1+num2))
+else:
+    print(int(num1/num2))
