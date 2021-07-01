083b86c2e316f696937f59c8272400bbd86e208e
Proton <surajpal2405@gmail.com>
2021-07-01 22:15:49 +0530

    modified for a condition n==4

--- print_the_pattern.py
+++ print_the_pattern.py
@@ -3,12 +3,12 @@ print("pattern printing")
 num=int(input("enter the number of rows you want :"))
 bool=input("1 for ascending 0 for descending")
 if bool=="1":
-    for i in range(1,num+1,1):
+    for i in range(0,num+1,1):
         print("*"*i)
+        if num == 4:
+            print("invalid input")
+            break
 else:
     for i in range(num,0,-1):
         print("*"*i)
 
-# for(int i=0;i<=5;i++){
-#   print("*"*i);
-# }
